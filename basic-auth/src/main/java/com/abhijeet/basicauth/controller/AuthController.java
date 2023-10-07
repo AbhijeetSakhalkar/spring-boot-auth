d81be5e3c8ba0eefe2346df03ae76a9186cb3fc9
Abhijeet Sakhalkar <sakhalkar123abj@gmail.com>
2023-10-07 12:15:15 +0530

    basic username password auth-refactored

--- basic-auth/src/main/java/com/abhijeet/basicauth/controller/AuthController.java
+++ basic-auth/src/main/java/com/abhijeet/basicauth/controller/AuthController.java
@@ -1,4 +1,4 @@
-package com.abhijeet.basicauth;
+package com.abhijeet.basicauth.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
